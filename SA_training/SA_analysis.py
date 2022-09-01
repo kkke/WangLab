ede5d87c7fdcf15f99b71f9e7fa3eef06840f420
kkke <llkong.ck@gmail.com>
2022-09-01 12:50:17 -0400

    Create SA_analysis.py

---
+++ SA_training/SA_analysis.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 31 17:12:03 2022
+
+@author: KeChen
+"""
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+wkdirectory = 'D:\Matlab_scripts\WangLab\SA_training'
+os.chdir(wkdirectory)
+
+%run medpc_read.py
+
+
+datadirectory = 'H:\Data\SA_training\SA_25'
+
+i = 0
+for filenames in os.listdir(datadirectory):
+    if filenames.endswith('.txt'):
+        # Prepare the Summary DataFrame for Each Session
+        temp= medpc_readdata(os.path.join(datadirectory, filenames))
+        
+    else:
+        continue
+    if i == 0:
+        data = temp
+    else:
+        data = pd.concat([data,  temp], ignore_index=True)
+    i = i+1
+
