4b34a158b1ffcbe98fe906cddd037c1733a2e9d0
kkke <llkong.ck@gmail.com>
2022-10-26 16:03:00 -0400

    Update whole_brain_cFos_moreregions.py

--- Whole-Brain_cFos/whole_brain_cFos_moreregions.py
+++ Whole-Brain_cFos/whole_brain_cFos_moreregions.py
@@ -14,7 +14,7 @@ Created on Mon Aug 29 11:48:23 2022
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import seaborn as sns
 # =============================================================================
 # directory = 'I:\LifeCanvas CRO datasets\May 2022 datasets\FanWang_03292022_MIT_analysis'
 # filename = 'combined_density_FanWang_03292022_MIT_6.csv'
@@ -25,7 +25,7 @@ import matplotlib.pyplot as plt
 # filename = 'combined_density_MIT_Wang_12142021_6.csv'
 
 directory = 'D:/WangLab_data/Data_Analysis_WholeBrain_cFos/rawData'
-filename = 'kcmp22-24Raw.csv'
+filename = 'kcmp40-41-46Raw.csv'
 os.chdir(directory)
 
 #%% extract useful information
@@ -37,7 +37,7 @@ cortical_data = rawData[(rawData.parent_structure_id == 5) & (rawData.depth == 6
 corticalplate_data = rawData[(rawData.parent_structure_id == 555) & (rawData.depth == 5)]
 striatum_data = rawData[(rawData.parent_structure_id > 571) & (rawData.parent_structure_id <= 592) & (rawData.depth == 6)]
 pallidum_data = rawData[(rawData.parent_structure_id > 609) & (rawData.parent_structure_id <= 620) & (rawData.depth == 6)]
-thalamus_data = rawData[(rawData.parent_structure_id >= 666) & (rawData.parent_structure_id <= 696) & (rawData.depth == 6)]
+thalamus_data = rawData[(rawData.parent_structure_id >= 642) & (rawData.parent_structure_id <= 696) & (rawData.depth == 6)]
 hypothalamus_data = rawData[(rawData.parent_structure_id >= 715) & (rawData.parent_structure_id <= 802) & (rawData.depth == 5)]
 midbrain_data = rawData[(rawData.parent_structure_id >= 807) & (rawData.parent_structure_id <= 868) & (rawData.depth == 5)]
 hindbrain_data = rawData[(rawData.parent_structure_id >= 884) & (rawData.parent_structure_id <= 924) & (rawData.depth == 6)]
@@ -48,13 +48,17 @@ frames = [cortical_data, corticalplate_data, striatum_data,
 sub_data = pd.concat(frames)
 
 sub_data.plot.bar(x = 'acronym', y = [6, 7,8], title = 'Left', xlabel = 'Regions', ylabel = 'cFos+ Density (cells/mm^3)')
+#%% data cleanning
+sub_data = sub_data.drop(sub_data.columns[[6,7,8,9,13]], axis = 1)
 
 
 #%% re-organize the data
 sub_data_transpose = sub_data.T
 sub_data_transpose.columns = sub_data_transpose.loc['acronym']
 sub_data_transpose = sub_data_transpose.drop(sub_data_transpose.index[0:6])
-sub_data_new = sub_data_transpose.rename(index=lambda s: s[3:9])
+
+
+sub_data_new = sub_data_transpose.rename(index=lambda s: s[0:6])
 
 treatmentData = pd.read_excel('D:/WangLab_data/Data_Analysis_WholeBrain_cFos/mouse_log.xlsx')
 gender = [];
@@ -74,3 +78,20 @@ files = filename[0:-7]
 os.chdir(summaryfolder)
 os.makedirs('./summary', exist_ok=True)
 sub_data_new.to_csv('./summary/summary_' + files + '.csv')
+
+#%% summarize all data together
+directory = 'D:/WangLab_data/Data_Analysis_WholeBrain_cFos/summary'
+os.chdir(directory)
+i = 0
+for f in os.listdir(directory):
+    temp = pd.read_csv(os.path.join(directory, f))
+    i +=1
+    if i ==1:
+        data = temp
+    else:
+        data = pd.concat([data,temp])
+        
+        
+
+
+
