8a5b5ef55dfff2e342914b016b005bea88ad68d3
kkke <llkong.ck@gmail.com>
2023-01-26 15:53:13 -0500

    Create psth_fb.m

---
+++ FB_analysis/psth_fb.m
@@ -0,0 +1,40 @@
+function [psth_time,psth_signal] = psth_fb(data,time, event, pre, post)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+%
+% data = Signal_concat;
+% time = Time_concat;
+% event = DIO.infusion;
+% pre = -5; % in secs
+% post= 5; % in secs
+sample_rate = 1/mean(diff(time(1:1000)));
+sample_size = floor((post-pre) * sample_rate);
+index = [];
+psth_signal = zeros(sample_size, length(event));
+psth_time   = zeros(sample_size, length(event));
+for i = 1:length(event)
+    index = find(time> event(i) + pre & time < event(i) + post);
+    if length(index)< sample_size
+    else
+        psth_time(:, i) = time(index(1:sample_size)) -  event(i);
+        psth_signal(:,i) = data(index(1:sample_size));
+    end
+end
+
+figure;
+subplot(2,1,1)
+plot(mean(psth_time, 2), mean(psth_signal, 2), 'k')
+hold on
+x = mean(psth_time, 2);
+y = mean(psth_signal, 2);
+e = std(psth_signal,1, 2)/sqrt(size(psth_signal, 2));
+boundedline(x, y, e, '-k');
+xlabel('Time (s)')
+ylabel('\Delta F/F')
+subplot(2, 1, 2)
+imagesc(mean(psth_time, 2),[] ,psth_signal')
+colorbar
+xlabel('Time (s)')
+ylabel('Trials')
+end
+
