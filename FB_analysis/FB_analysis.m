6c584bd5fe04e8bc555c044301173ca265a6c477
kkke <llkong.ck@gmail.com>
2023-02-18 22:44:22 -0500

    Update FB_analysis.m

--- FB_analysis/FB_analysis.m
+++ FB_analysis/FB_analysis.m
@@ -3,6 +3,7 @@ filename = 'SA57_NAc_011923_0001';
 % Data_Acquired = ExtractDataAcquisition([filename, '.doric');
 ProcessedData_filename = {[filename, '_DFF001'], [filename, '_DFF002'], [filename, '_DFF003']};
 rawdata_tree = '/DataAcquisition/FPConsole/Signals/';
+setting_tree = '/Configurations/FPConsole/AIN02/Settings/';
 processed_data_tree = '/DataProcessed/FPConsole/DFFSignals/';
 Seriers = {'Series0001', 'Series0002', 'Series0003'};
 EventVariables = {'DataAcquisition_FPConsole_Signals_Series0001_DigitalIO', ...
@@ -11,6 +12,8 @@ EventVariables = {'DataAcquisition_FPConsole_Signals_Series0001_DigitalIO', ...
 
 for i = 1:length(Seriers)
     ProcessedData(i).Seriers = Seriers{i};
+    ProcessedData(i).settings.Sig405_output  = h5readatt([filename, '.doric'], [setting_tree, 'LockInAOUT1'], 'OtputLevel');
+    ProcessedData(i).settings.Sig470_output  = h5readatt([filename, '.doric'], [setting_tree, 'LockInAOUT2'], 'OtputLevel');    
     ProcessedData(i).rawData.Sig405   = h5read([filename, '.doric'], [rawdata_tree, Seriers{i}, '/AIN02xAOUT01-LockIn/Values']);
     ProcessedData(i).rawData.Sig470   = h5read([filename, '.doric'], [rawdata_tree, Seriers{i}, '/AIN02xAOUT02-LockIn/Values']);
     ProcessedData(i).rawData.time     = h5read([filename, '.doric'], [rawdata_tree, Seriers{i}, '/AIN02xAOUT02-LockIn/Time']);
@@ -36,4 +39,44 @@ h5 = plot(ProcessedData(i).processed.time, ProcessedData(i).processed.signal)
 legend({'Lever', 'Infusion', 'Front', 'Back', 'DA'})
 
 %% get the timestamps of each event.
-
+for i = 1:length(ProcessedData)
+    rawDIO = ProcessedData(i).rawDIO;
+    [ProcessedData(i).DIO.lever_in, ProcessedData(i).DIO.lever_out]=Timing_onset_offset(rawDIO.lever, rawDIO.time, 0.5,1,0);
+    ProcessedData(i).DIO.infusion = med_find_event(rawDIO.infusion, rawDIO.time);
+    ProcessedData(i).DIO.front = med_find_event(rawDIO.front, rawDIO.time);
+    ProcessedData(i).DIO.back = med_find_event(rawDIO.back, rawDIO.time);  
+    figure
+    plot(ProcessedData(i).rawDIO.time, ProcessedData(i).rawDIO.lever* -1 + 1)
+    hold on
+    plot(ProcessedData(i).rawDIO.time, ProcessedData(i).rawDIO.infusion* -1 + 1)
+    plot(ProcessedData(i).rawDIO.time, ProcessedData(i).rawDIO.front*-1)
+    plot(ProcessedData(i).rawDIO.time, ProcessedData(i).rawDIO.back*-1)
+    plot(ProcessedData(i).processed.time, ProcessedData(i).processed.signal)
+    legend({'Lever', 'Infusion', 'Front', 'Back', 'DA'})
+    scatter(ProcessedData(i).DIO.lever_out, ones(size(ProcessedData(i).DIO.lever_out)))
+    scatter(ProcessedData(i).DIO.infusion, ones(size(ProcessedData(i).DIO.infusion)))
+    scatter(ProcessedData(i).DIO.front, ones(size(ProcessedData(i).DIO.front)))
+    scatter(ProcessedData(i).DIO.back , ones(size(ProcessedData(i).DIO.back)))
+end
+%% plot the psth for fb
+% concatenate the data
+Signal_concat = [ProcessedData(1).processed.signal; ProcessedData(2).processed.signal; ProcessedData(3).processed.signal];
+Time_concat   = [ProcessedData(1).processed.time; ProcessedData(2).processed.time; ProcessedData(3).processed.time];
+for i = 1:length(ProcessedData)
+    if i ==1 
+        DIO = ProcessedData(1).DIO;
+    else
+        fields = fieldnames(DIO);
+        for k = 1:length(fields)
+            DIO.(fields{k}) = [DIO.(fields{k}); ProcessedData(i).DIO.(fields{k})];
+        end
+    end
+end
+figure; plot(Time_concat, Signal_concat)
+hold on
+scatter(DIO.lever_out, ones(size(DIO.lever_out)))
+scatter(DIO.infusion, ones(size(DIO.infusion)))
+scatter(DIO.front, ones(size(DIO.front)))
+scatter(DIO.back , ones(size(DIO.back)))
+%%
+data = Signal_concat
