f9ea90a3b0f99278977f2249910daf27af020fc3
kkke <llkong.ck@gmail.com>
2025-05-20 14:39:25 -0400

    Update bar_scatter_cluster.m

--- bar_scatter_cluster.m
+++ bar_scatter_cluster.m
@@ -8,7 +8,13 @@ for i = 1:length(data_bar)
     sem_value = std(data_bar{i})./sqrt(length(data_bar{i}));
     b = bar(i, mean_value);
     b.FaceColor = 'flat';
-    b.CData(1,:) = colors(i, :);
+    switch i 
+        case 1
+    b.CData(1,:) = colors(2, :);
+        case 2
+    b.CData(1,:) = colors(3, :);
+    end
+    b.FaceAlpha = 0.4;
     hold on
     errorbar(i, mean_value, sem_value,'k.', 'LineWidth', 1, 'CapSize',10)
 end
@@ -16,24 +22,28 @@ for i = 1:4
     switch i
         case 1
 
-            scatter(i + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'k', 'MarkerFaceColor',colors(i,:))
+            scatter(i + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'MarkerEdgeColor', 'none', 'MarkerFaceColor',colors(2,:))
         case 2
-            scatter(i + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'k', 'MarkerFaceColor',colors(i,:))
+            scatter(i + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'MarkerEdgeColor', 'none', 'MarkerFaceColor',colors(3,:))
         case 3
-            scatter(1 + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'k', 'MarkerFaceColor',colors(i,:))
+            scatter(1 + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'MarkerEdgeColor', 'none', 'MarkerFaceColor',colors(2,:))
         case 4
-            scatter(2 + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'k', 'MarkerFaceColor',colors(i,:))
+            scatter(2 + 0.3* (rand(size(fano_value(find(clusterID == i))))-0.5), fano_value(find(clusterID == i)), 'MarkerEdgeColor', 'none', 'MarkerFaceColor',colors(3,:))
 
     end
 end
-xlabel('Cluster #');
+xlabel('Drug Taking', 'FontSize', 14);
 ylabel('Infusion #');
 box off
 set(gca,'TickDir','out')
 set(gca,'fontsize',12)
 set(gca,'TickLengt', [0.015 0.015]);
 set(gca, 'LineWidth',1)
-set(gcf,'position',[100,100,200,340])
-set(gca,'XTick',[1, 2, 3, 4])
+set(gcf,'position',[100,100,150,250])
+% set(gca,'XTick',[1, 2, 3, 4])
+xticks([1 2]);
+xticklabels({'High', 'Low'});
 ylabel('Fano Factor')
 ylim([0, 5])
+
+[h,p] = ttest2(data_bar{1}, data_bar{2})
